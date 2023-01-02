4d9c0c83decba1317a13cedc4d3e103a0c038ab4
Liliana Lessa <24816655+LilianaLessa@users.noreply.github.com>
2023-01-02 17:34:54 +0100

    styling views

--- App.js
+++ App.js
@@ -1,9 +1,34 @@
-import { Text, View } from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {StatusBar} from 'react-native';
+import React from "react";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center"}}>
-      <Text>Our react native blank canvas</Text>
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Text>search</Text>
+        </View>
+        <View style={styles.list}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <StatusBar style="auto" />
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  search: {
+    padding: 16,
+    backgroundColor: "green",
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
+  }
+});
