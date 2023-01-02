f4b2f248ed0b3871cd98132a0fce9afccb28df6b
Liliana Lessa <24816655+LilianaLessa@users.noreply.github.com>
2023-01-02 22:49:34 +0100

    creating Restaurant List feature

--- App.js
+++ App.js
@@ -1,34 +1,12 @@
-import { StatusBar, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "react-native";
 import React from "react";
-import { Searchbar } from "react-native-paper";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar />
-        </View>
-        <View style={styles.list}>
-          <Text>list</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <StatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  search: {
-    padding: 16,
-    //backgroundColor: "green",
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue",
-  },
-});
