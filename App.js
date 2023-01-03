acdcaabf7522ba1a0ae3f5e1eb1d01e7d66cfa3d
Liliana Lessa <24816655+LilianaLessa@users.noreply.github.com>
2023-01-03 21:29:07 +0100

    setup theme and poc

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./src/infrastructure/theme";
 
 export default function App() {
   return (
     <>
-      <RestaurantsScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
     </>
   );
 }
