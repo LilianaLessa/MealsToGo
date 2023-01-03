e3e10e05445571fbdf1d6d32793f139de414b0c8
Liliana Lessa <24816655+LilianaLessa@users.noreply.github.com>
2023-01-03 21:48:08 +0100

    using fonts

--- App.js
+++ App.js
@@ -3,7 +3,17 @@ import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurant
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
 
+import { useFonts, Oswald_400Regular } from "@expo-google-fonts/oswald";
+import { Lato_400Regular } from "@expo-google-fonts/lato";
+
 export default function App() {
+  const [oswaldLoaded] = useFonts({ Oswald_400Regular });
+  const [latoLoaded] = useFonts({ Lato_400Regular });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
