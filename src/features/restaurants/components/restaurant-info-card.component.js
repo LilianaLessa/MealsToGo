e415fce5f2205e31e1b93ad6f7749923119bd56a
Liliana Lessa <24816655+LilianaLessa@users.noreply.github.com>
2023-01-07 23:30:02 +0100

    new property on restaurant data transformer

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -22,7 +22,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     photos = [
       "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
-    address = restaurant.vicinity,
+    address = "100 some random street",
     isOpenNow = true,
     rating = 4,
     isClosedTemporarily = true,
