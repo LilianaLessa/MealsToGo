73cd0d8e3fd7984f976154c71db4fd017b58fee4
Liliana Lessa <24816655+LilianaLessa@users.noreply.github.com>
2023-01-08 16:01:38 +0100

    showing restaurant makers on map

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import styled from "styled-components/native";
 import { Search } from "../components/search.component";
 import { LocationContext } from "../../../services/location/location.context";
@@ -34,8 +34,17 @@ export const MapScreen = () => {
           longitudeDelta: 0.02,
         }}
       >
-        {restaurants.map(() => {
-          return null;
+        {restaurants.map((restaurant) => {
+          return (
+            <Marker
+              key={restaurant.name}
+              title={restaurant.name}
+              coordinate={{
+                latitude: restaurant.geometry.location.lat,
+                longitude: restaurant.geometry.location.lng,
+              }}
+            />
+          );
         })}
       </Map>
     </>
