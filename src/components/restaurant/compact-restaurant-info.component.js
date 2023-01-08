05ef17488178667174a08baf92a0e343f3d73186
Liliana Lessa <24816655+LilianaLessa@users.noreply.github.com>
2023-01-08 16:45:22 +0100

    setting up marker callout

---
+++ src/components/restaurant/compact-restaurant-info.component.js
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Text } from "../typography/text.component";
+import WebView from "react-native-webview";
+import { Platform } from "react-native";
+
+const CompactImage = styled.Image`
+  border-radius: 10px;
+  width: 120px;
+  height: 100px;
+`;
+
+const CompactWebView = styled(WebView)`
+  border-radius: 10px;
+  width: 120px;
+  height: 100px;
+`;
+
+const Item = styled.View`
+  padding: 10px;
+  max-width: 120px;
+  align-items: center;
+`;
+
+const isAndroid = Platform.OS === "android";
+export const CompactRestaurantInfo = ({ restaurant }) => {
+  const {
+    name,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+  } = restaurant;
+
+  const Image = isAndroid ? CompactWebView : CompactImage;
+
+  return (
+    <Item>
+      <Image source={{ uri: photos[0] }} />
+      <Text center variant="caption" numberOfLines={3}>
+        {name}
+      </Text>
+    </Item>
+  );
+};
